80d668b188c90135cff063f7b8c1660752733237
bapusahebg1982 <bapusahebg.1982@gmail.com>
2026-04-29 20:59:17 -0400

    Create models.py

---
+++ backend/database/models.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String
+from database.db import Base
+
+class Stock(Base):
+    __tablename__ = "stocks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    ticker = Column(String, unique=True, index=True)
+    market = Column(String)
